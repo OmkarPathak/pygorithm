4df07bab298dc8d22232da3caaaf1075d958f4a2
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-01 06:32:54 +0530

    Added quick sort

---
+++ pygorithms/sorting/quick_sort.py
@@ -0,0 +1,29 @@
+# Author: OMKAR PATHAK
+# Created On: 31st July 2017
+
+#  Best = Average = O(nlog(n)), Worst = O(n ^ 2)
+
+# quick_sort algorithm
+def sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return sort(left) + middle + sort(right)
+
+# time complexities
+def bestcase_complexity():
+    return 'O(nlogn)'
+
+def averagecase_complexity():
+    return 'O(nlogn)'
+
+def worstcase_complexity():
+    return 'O(n ^ 2)'
+
+# easily retrieve the source code of the sort function
+def get_code():
+    import inspect
+    return inspect.getsource(sort)
