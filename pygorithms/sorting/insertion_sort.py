2b0278dd77e634a7bc8f332411a9a97225ac284a
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-01 06:32:31 +0530

    Added insertion sort

---
+++ pygorithms/sorting/insertion_sort.py
@@ -0,0 +1,31 @@
+# Author: OMKAR PATHAK
+# Created On: 31st July 2017
+
+#  Best O(n); Average O(n^2); Worst O(n^2)
+
+# insertion sort algorithm
+def sort(List):
+    for i in range(1, len(List)):
+        currentNumber = List[i]
+        for j in range(i - 1, -1, -1):
+            if List[j] > currentNumber :
+                List[j], List[j + 1] = List[j + 1], List[j]
+            else:
+                List[j + 1] = currentNumber
+                break
+    return List
+
+# time complexities
+def bestcase_complexity():
+    return 'O(n)'
+
+def averagecase_complexity():
+    return 'O(n ^ 2)'
+
+def worstcase_complexity():
+    return 'O(n ^ 2)'
+
+# easily retrieve the source code of the sort function
+def get_code():
+    import inspect
+    return inspect.getsource(sort)
