fdc099145259cb62d611421e7ea3469fbe2e9e61
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-01 06:31:44 +0530

    Added bubble sort

---
+++ pygorithms/sorting/bubble_sort.py
@@ -0,0 +1,27 @@
+# Author: OMKAR PATHAK
+# Created On: 31st July 2017
+
+# Best O(n^2); Average O(n^2); Worst O(n^2)
+
+# Bubble Sorting algorithm
+def sort(List):
+    for i in range(len(List)):
+        for j in range(len(List) - 1, i, -1):
+            if List[j] < List[j - 1]:
+                List[j], List[j - 1] = List[j - 1], List[j]
+    return List
+
+# time complexities
+def bestcase_complexity():
+    return 'O(n ^ 2)'
+
+def averagecase_complexity():
+    return 'O(n ^ 2)'
+
+def worstcase_complexity():
+    return 'O(n ^ 2)'
+
+# easily retrieve the source code of the sort function
+def get_code():
+    import inspect
+    return inspect.getsource(sort)
