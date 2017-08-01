85ae40e32b9323e8cc8f25f1d0425aaf34aea125
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-01 06:33:17 +0530

    Added shell sort

---
+++ pygorithms/sorting/shell_sort.py
@@ -0,0 +1,34 @@
+# Author: OMKAR PATHAK
+# Created On: 31st July 2017
+
+# Best Case O(n logn); Average Case O(depends on gap sequence); Worst Case O(n)
+
+# shell sort algorithm
+def sort(myList):
+    gap = len(myList) // 2
+    while gap > 0:
+        for i in range(gap, len(myList)):
+            currentItem = myList[i]
+            j = i
+            while j >= gap and myList[j - gap] > currentItem:
+                myList[j] = myList[j - gap]
+                j -= gap
+            myList[j] = currentItem
+        gap //= 2
+
+    return myList
+
+# time complexities
+def bestcase_complexity():
+    return 'O(nlogn)'
+
+def averagecase_complexity():
+    return 'O(depends on gap sequence)'
+
+def worstcase_complexity():
+    return 'O(n)'
+
+# easily retrieve the source code of the sort function
+def get_code():
+    import inspect
+    return inspect.getsource(sort)
