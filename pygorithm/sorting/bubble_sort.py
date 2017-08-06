8caa96ee10cd57208ac1c432ef7c54156c2a9e13
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2017-08-06 10:16:55 +0100

    Updating
    If bubble sort does a full loop without any change to list, it returns.

--- pygorithm/sorting/bubble_sort.py
+++ pygorithm/sorting/bubble_sort.py
@@ -1,19 +1,24 @@
 # Author: OMKAR PATHAK
+# Contributors: Mohamed Kiouaz
 # Created On: 31st July 2017
 
-# Best O(n^2); Average O(n^2); Worst O(n^2)
+# Best O(n); Average O(n*(n-1)/4); Worst O(n^2)
 
 # Bubble Sorting algorithm
 def sort(List):
     for i in range(len(List)):
+        stop = True
         for j in range(len(List) - 1, i, -1):
             if List[j] < List[j - 1]:
+                stop = False
                 List[j], List[j - 1] = List[j - 1], List[j]
+        if(stop == True):
+            return List
     return List
 
 # time complexities
 def time_complexities():
-    return '''Best Case: O(n ^ 2), Average Case: O(n ^ 2), Worst Case: O(n ^ 2)'''
+    return '''Best case O(n); Average case O(n * (n - 1) / 4); Worst case O(n ^ 2)'''
 
 # easily retrieve the source code of the sort function
 def get_code():
