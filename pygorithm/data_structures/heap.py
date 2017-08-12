ecee52f27d33905d8eb12180a1dd95f9a8311c73
Allston Mickey <allston.mickey@gmail.com>
2017-08-12 01:04:20 -0700

    Implement pop for heap.

--- pygorithm/data_structures/heap.py
+++ pygorithm/data_structures/heap.py
@@ -33,3 +33,46 @@ class Heap(Queue):
             self.queue[child], self.queue[parent] = self.queue[parent], self.queue[child]
             child = parent
             parent = self.parent_idx(child)
+
+    def pop(self):
+        min = self.dequeue()
+        if self.rear >= 1:
+            self.heapify_down()
+
+    def favorite(self, parent):
+        """
+        Determines which child has the highest priority by 3 cases.
+        """
+        left  = self.left_child_idx(parent)
+        right = self.right_child_idx(parent)
+        
+        # case 1: both nodes exist
+        if left <= self.rear and right <= self.rear:
+            if self.queue[left] <= self.queue[right]: # left gets priority
+                return left
+            else: # right gets priority
+                return right
+        # case 2: only left exists
+        elif left <= self.rear: # only left exists
+            return left
+        # case 3: no children (if left doesn't exist, neither can the right)
+        else:
+            return None # no child exists
+        
+    def heapify_down(self):
+        """
+        Select the root and sift down until min-heap property is met.
+
+        While a favorite child exists, and that child is smaller
+        than the parent, swap them (sift down).
+        """
+        cur = ROOT = 0
+        fav = self.favorite(cur)
+        while self.queue[fav] is not None:
+            if self.queue[cur] > self.queue[fav]:
+                fav = self.favorite(cur)
+                self.queue[cur], self.queue[fav] = self.queue[fav], self.queue[cur]
+                cur = fav
+            else:
+                return
+
