324b62362782f0a89574c0b25c0fc59b156e0b36
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-12 06:59:21 +0530

    Made PEP8 compatible

--- pygorithm/data_structures/queue.py
+++ pygorithm/data_structures/queue.py
@@ -3,15 +3,6 @@
 
 # queue implementation
 class Queue(object):
-    '''
-
-    size    : return the current size of the queue
-    enqueue : insert an item into the queue
-    dequeue : remove an item from the queue which was first inserted
-    isEmpty : check if the queue is empty
-
-    '''
-
     def __init__(self, limit = 10):
         '''
         @param: limit: queue size
@@ -22,16 +13,19 @@ class Queue(object):
         self.limit = limit
         self.size = 0
 
-    # for printing the contents of the queue
     def __str__(self):
         return ' '.join([str(i) for i in self.queue])
 
-    # to check if queue is empty
-    def isEmpty(self):
+    def size(self):
+        ''' returns the current size of the queue '''
+        return self.size
+
+    def is_empty(self):
+        ''' checks if the queue is empty '''
         return self.size <= 0
 
-    # to add an element from the rear end of the queue
     def enqueue(self, data):
+        ''' inserts an item into the queue '''
         if self.size >= self.limit:
             return -1          # queue overflow
         else:
@@ -45,9 +39,9 @@ class Queue(object):
 
         self.size += 1
 
-    # to pop an element from the front end of the queue
     def dequeue(self):
-        if self.isEmpty():
+        ''' pops an item from the queue which was first inserted '''
+        if self.is_empty():
             return -1          # queue underflow
         else:
             self.size -= 1
@@ -57,27 +51,11 @@ class Queue(object):
                 self.rear = self.size - 1
             return self.queue.pop(0)
 
-    # return the size of the queue
-    def size(self):
-        return self.size
-
-    # easily retrieve the source code of the Queue class
     def get_code(self):
         import inspect
         return inspect.getsource(Queue)
 
 class Deque(object):
-    '''
-
-    isEmpty     : checks whether the deque is empty
-    isFull      : checks whether the deque is full
-    insertRear  : inserts an element at the rear end of the deque
-    insertFront : inserts an element at the front end of the deque
-    deleteRear  : deletes an element from the rear end of the deque
-    deleteFront : deletes an element from the front end of the deque
-
-    '''
-
     def __init__(self, limit = 10):
         self.queue = []
         self.limit = limit
@@ -85,38 +63,43 @@ class Deque(object):
     def __str__(self):
         return ' '.join([str(i) for i in self.queue])
 
-    # check if queue is empty
-    def isEmpty(self):
+    def is_empty(self):
+        ''' checks whether the deque is empty '''
         return len(self.queue) <= 0
 
-    # check if queue is full
-    def isFull(self):
+    def is_full(self):
+        ''' checks whether the deque is full '''
         return len(self.queue) >= self.limit
 
-    # for inserting at rear
-    def insertRear(self, data):
-        if self.isFull():
+    def insert_rear(self, data):
+        ''' inserts an element at the rear end of the deque '''
+        if self.is_full():
             return
         else:
             self.queue.insert(0, data)
 
-    # for inserting at front end
-    def insertFront(self, data):
-        if self.isFull():
-            return
+    def insert_front(self, data):
+        ''' inserts an element at the front end of the deque '''
+        if self.is_full():
+            return -1
         else:
             self.queue.append(data)
 
-    # deleting from rear end
-    def deleteRear(self):
-        if self.isEmpty():
-            return
+    def delete_rear(self):
+        ''' deletes an element from the rear end of the deque '''
+        if self.is_empty():
+            return -1
         else:
             return self.queue.pop(0)
 
-    # deleting from front end
-    def deleteFront(self):
-        if self.isFull():
+    def delete_front(self):
+        ''' deletes an element from the front end of the deque '''
+        if self.is_full():
             return
         else:
             return self.queue.pop()
+
+    def get_code(self):
+        ''' returns the code of the current class '''
+        import inspect
+        return inspect.getsource(Deque)
