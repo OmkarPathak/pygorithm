4f380308aef800311e0e481b6271cfeb6b16b645
ashu01 <as01hu@gmail.com>
2017-08-15 15:48:50 +0530

    added in Unit test module and bug fixed

--- pygorithm/fibonacci/space_optimized.py
+++ pygorithm/fibonacci/space_optimized.py
@@ -27,7 +27,7 @@ def get_sequence(n):
         if n == 1:
             return b
 
-        for i in range(2, n):
+        for i in range(1, n):
             c = a + b
             a = b
             b = c
