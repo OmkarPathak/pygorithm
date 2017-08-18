0ca5ef8656533165627f270ff43a13c1ace7082a
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-18 06:43:25 +0530

    Added __init__ and modules.py

---
+++ pygorithm/string/__init__.py
@@ -0,0 +1,15 @@
+"""
+Collection of string methods and functions
+"""
+from . import anagram
+from . import pangram
+from . import isogram
+from . import modules
+from . import palindrome
+
+__all__ = [
+    'anagram',
+    'pangram',
+    'isogram',
+    'palindrome'
+]
