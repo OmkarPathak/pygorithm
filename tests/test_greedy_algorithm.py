5a0946ee2ae4f327a9adfa558522a6a9485bb0f6
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-26 06:25:37 +0530

    Added activity selection problem implementation and its tests

--- tests/test_greedy_algorithm.py
+++ tests/test_greedy_algorithm.py
@@ -3,6 +3,7 @@ import unittest
 
 from pygorithm.greedy_algorithm import (
     fractional_knapsack,
+    activity_selection,
     )
 
 
@@ -12,6 +13,13 @@ class TestFractionalKnapsack(unittest.TestCase):
         weight = [10, 20, 30]
         W = 50
         self.assertEqual(fractional_knapsack.knapsack(W, value, weight), 240)
+
+class TestActivitySelectionProblem(unittest.TestCase):
+    def test_activity_selection(self):
+        start_times = [1 , 3 , 0 , 5 , 8 , 5]
+        finish_times = [2 , 4 , 6 , 7 , 9 , 9]
+
+        self.assertEqual(activity_selection.activity_selection(start_times, finish_times), [0, 1, 3, 4])
 
 if __name__ == '__main__':
     unittest.main()
