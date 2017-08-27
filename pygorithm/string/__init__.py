a286969940dfc07d77519ae052e15f5ac54506d6
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-27 05:59:44 +0530

    Added Manachers Algorithm implementation

--- pygorithm/string/__init__.py
+++ pygorithm/string/__init__.py
@@ -5,10 +5,12 @@ from . import anagram
 from . import pangram
 from . import isogram
 from . import palindrome
+from . import manacher_algorithm
 
 __all__ = [
     'anagram',
     'pangram',
     'isogram',
+    'manacher_algorithm',
     'palindrome'
 ]
