5c78361021a2b54dca260402609d0dc4bcb585a1
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-29 08:06:34 +0530

    Modified Dosctrings

--- pygorithm/dynamic_programming/binary_knapsack.py
+++ pygorithm/dynamic_programming/binary_knapsack.py
@@ -3,11 +3,16 @@ Author: Omkar Pathak
 Created At: 25th August 2017
 """
 import inspect
-# TODO: Explain how this works / Explain what a knapsack is
 
 
 def knapsack(w, value, weight):
     """
+    The knapsack problem or rucksack problem is a problem in combinatorial optimization: Given a set of
+    items, each with a weight and a value, determine the number of each item to include in a collection
+    so that the total weight is less than or equal to a given limit and the total value is as large as
+    possible. It derives its name from the problem faced by someone who is constrained by a fixed-size
+    knapsack and must fill it with the most valuable items.
+
     :param w: maximum weight capacity
     :param value: an array of values of items in the knapsack
     :param weight: an array of weights of items in the knapsack
