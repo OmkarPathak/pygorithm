74a80817474c3f2fcd5f74ab62adfe4969c614cb
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-30 07:23:46 +0530

    Typo fixes

--- pygorithm/binary/binary_utils.py
+++ pygorithm/binary/binary_utils.py
@@ -4,7 +4,14 @@ Author: Ian Doarn
 """
 
 
-def pad(value: str, return_type=str) -> """Pad binary value with zeros""":
+def pad(value, return_type):
+    '''
+        Pad binary value with zeros
+        :param value: string
+        :param return_type: string
+    '''
+    if type(value) is not str:
+        raise TypeError("pad only accepts str, not {}".format(str(type(value))))
     if len(value) % 4 != 0:
         pad_amount = 4 - (len(value) % 4)
         return return_type(('0' * pad_amount) + value)
@@ -12,5 +19,10 @@ def pad(value: str, return_type=str) -> """Pad binary value with zeros""":
         return return_type(value)
 
 
-def to_string(binary_array: list, delimiter=' ') -> """Convert binary array to string""":
+def to_string(binary_array, delimiter=' '):
+    """
+        Convert binary array to string
+    """
+    if type(binary_array) is not list:
+        raise TypeError("to_string only accepts lists, not {}".format(str(type(value))))
     return delimiter.join(binary_array)
