dffa590e7ad28b2bb991dd84ea0aadca6d9ae50c
Abhay Raj Singh <abhayonlyone@gmail.com>
2019-10-06 16:05:26 +0000

    Optimised and simplified lcs.py (#92)

--- pygorithm/dynamic_programming/lcs.py
+++ pygorithm/dynamic_programming/lcs.py
@@ -28,19 +28,16 @@ def longest_common_subsequence(s1, s2):
     :param s2: string
     :return: int
     """
-    m = len(s1)
-    n = len(s2)
+    m, n = len(s1), len(s2)
 
-    dp = [[0] * (n + 1) for i in range(m + 1)]
+    dp = [[0] * (n + 1)] * (m + 1)
     """
     dp[i][j] : contains length of LCS of s1[0..i-1] and s2[0..j-1]
     """
 
-    for i in range(m + 1):
-        for j in range(n + 1):
-            if i == 0 or j == 0:
-                dp[i][j] = 0
-            elif s1[i - 1] == s2[j - 1]:
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1] + 1
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
